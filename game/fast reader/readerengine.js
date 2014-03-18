7ababaf9f69f590ce1f4c027b4cd179eec2718c4
Miguel Rosende <rosende_bustamante@yahoo.com.ar>
2014-03-18 13:47:41 -0300

    copy of game: the functions and it's attributes were changed new pages were added

---
+++ game/fast reader/readerengine.js
@@ -0,0 +1,5 @@
+﻿$(document).ready(function () {
+    $('.scroll').animate({ right: $(document).width() 
+    }, 30000); 
+
+});
---
+++ game/todo.js
@@ -0,0 +1,36 @@
+﻿function TodoCtrl($scope) {
+    $scope.todos = [
+        { text: 'learn javascript', done: false },
+        { text: 'learn angular', done: false },
+        { text: 'play dragon nest', done: false }
+    ];
+
+    $scope.addTodo = function () {
+        $scope.todos.push({ text:$scope.todoText, done: false});
+        $scope.todoText = "";
+    };
+
+    $scope.removeTodo = function () {
+        $scope.todos = _.filter($scope.todos, function (todo) {
+            return !todo.done;
+        })
+    };
+
+    $scope.remaining = function () {
+        var count = 0;
+        angular.forEach($scope.todos, function (todo) {
+            count += todo.done ? 0 : 1;
+        });
+        return count;
+    };
+
+    $scope.archive = function () {
+        var oldTodos = $scope.todos;
+        $scope.todos = [];
+        angular.forEach(oldTodos, function (todo) {
+            if (!todo.done)
+                $scope.todos.push(todo);
+        });
+    };
+        
+}
